0f6a696b192db4004408100d32c4825990572b34
LucaBn <>
2021-02-17 21:49:05 +0100

    spelling and indentation

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # jsutilities
 JS utilities  
 
-Most of these functions/mothods require JQuery to work  
+Most of these functions/methods require JQuery to work  
 Each time you read if(variable === undefined) is because of IE  
-Always use var because of IE  
+Always use var because of IE (ES6 is not supported)
 Classes used in JS file should be used just to trigger certain actions, so don't use them to apply CSS  
--- general.js
+++ general.js
@@ -1,7 +1,7 @@
 /*
-    Most of these functions/mothods require JQuery to work
-    Each time you read if(variable === undefined) is because of IE
-    Always use var because of IE
+	Most of these functions/mothods require JQuery to work
+	Each time you read if(variable === undefined) is because of IE
+	Always use var because of IE
 */
 
 // Detect if element is in viewport
@@ -21,30 +21,30 @@ $.fn.isInViewport = function(margin) {
 
 // Back to top/bottom animation
 var scroll_to_position = {
-    starting_position: 0,
-    animate: function(margin, speed) {
-        $('html, body').animate({scrollTop: margin}, speed);
-    },
-    back_to_top: function(margin, speed) {
-        if(margin === undefined) {
-            margin = 0;
-        }
-        if(speed === undefined) {
-            speed = "medium";
-        }
-        scroll_to_position.starting_position = window.scrollY;
-        animate(margin, speed);
-    },
-    back_to_bottom: function(margin, speed) {
-        if(margin === undefined) {
-            margin = 0;
-        }
-        if(speed === undefined) {
-            speed = "medium";
-        }
-        // When you call scroll_to_position.back_to_top method starting position is saved in scroll_to_position.starting_position, it can be used to go back to that point
-        animate((scroll_to_position.starting_position + margin), speed);
-    }
+	starting_position: 0,
+	animate: function(margin, speed) {
+		$('html, body').animate({scrollTop: margin}, speed);
+	},
+	back_to_top: function(margin, speed) {
+		if(margin === undefined) {
+			margin = 0;
+		}
+		if(speed === undefined) {
+			speed = "medium";
+		}
+		scroll_to_position.starting_position = window.scrollY;
+		animate(margin, speed);
+	},
+	back_to_bottom: function(margin, speed) {
+		if(margin === undefined) {
+			margin = 0;
+		}
+		if(speed === undefined) {
+			speed = "medium";
+		}
+		// When you call scroll_to_position.back_to_top method starting position is saved in scroll_to_position.starting_position, it can be used to go back to that point
+		animate((scroll_to_position.starting_position + margin), speed);
+	}
 }
 $("body").on("click", ".back-to-top", function() {
 	scroll_to_position.back_to_top();
@@ -94,91 +94,91 @@ var urls_handler = {
 }
 
 var gl = {
-    // Detect if tablet
-    is_tablet: /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(navigator.userAgent.toLowerCase()),
+	// Detect if tablet
+	is_tablet: /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(navigator.userAgent.toLowerCase()),
 
-    // Detect if IE
-    is_ie: !!document.documentMode,
+	// Detect if IE
+	is_ie: !!document.documentMode,
 
-    // Lazy load: loads images when they get close to viewport (or if they should be already visible when the page loads), called on scroll/resize
-    lazy_load_picture: function() {
-        $('img[realsrc]').each(function() {
-            if($(this).isInViewport() || $(this).isInViewport(-500)) {
-                var t = $(this);
-                t.attr('src', t.attr('realsrc'));
-                t.removeAttr('realsrc');
-                t.onload = console.log(t);
-            }
-        });
-    },
+	// Lazy load: loads images when they get close to viewport (or if they should be already visible when the page loads), called on scroll/resize
+	lazy_load_picture: function() {
+		$('img[realsrc]').each(function() {
+			if($(this).isInViewport() || $(this).isInViewport(-500)) {
+				var t = $(this);
+				t.attr('src', t.attr('realsrc'));
+				t.removeAttr('realsrc');
+				t.onload = console.log(t);
+			}
+		});
+	},
 
-    // Copy to clipboard
-    copy_to_clipboard: function(str_to_topy) {
-        var dummy = document.createElement("textarea");
-        document.body.appendChild(dummy).style.position="fixed";
-        dummy.value = str_to_topy;
-        dummy.select();
-        document.execCommand("copy");
-        document.body.removeChild(dummy);
-        // console.log("Copied!");
-    },
+	// Copy to clipboard
+	copy_to_clipboard: function(str_to_topy) {
+		var dummy = document.createElement("textarea");
+		document.body.appendChild(dummy).style.position="fixed";
+		dummy.value = str_to_topy;
+		dummy.select();
+		document.execCommand("copy");
+		document.body.removeChild(dummy);
+		// console.log("Copied!");
+	},
 
-    random_number: function(from, to) {
-        if(from === undefined) {
-            from = 0;
-        }
-        if(to === undefined) {
-            to = from + 10;
-        }
+	random_number: function(from, to) {
+		if(from === undefined) {
+			from = 0;
+		}
+		if(to === undefined) {
+			to = from + 10;
+		}
 
-        return Math.floor(Math.random()*(to-from+1)+from);
-    },
+		return Math.floor(Math.random()*(to-from+1)+from);
+	},
 
-    random_element_from_array: function(array) {
-        return array[Math.floor(Math.random()*array.length)];
-    },
+	random_element_from_array: function(array) {
+		return array[Math.floor(Math.random()*array.length)];
+	},
 
-    shuffle_array: function(array) {
-        var currentIndex = array.length, temporaryValue, randomIndex;
-    
-        // While there remain elements to shuffle
-        while (0 !== currentIndex) {
-            // Pick a remaining element
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-    
-            // And swap it with the current element
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
-    
-        return array;
-    },
+	shuffle_array: function(array) {
+		var currentIndex = array.length, temporaryValue, randomIndex;
 
-    check_key: function(e) {
-        e = e || window.event;
+		// While there remain elements to shuffle
+		while (0 !== currentIndex) {
+			// Pick a remaining element
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
 
-        // Usage example
-        if (e.keyCode == '38') {
-            // Up arrow clicked
-        }
-    },
+			// And swap it with the current element
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
 
-    /* Ajax call */
+		return array;
+	},
+
+	check_key: function(e) {
+		e = e || window.event;
+
+		// Usage example
+		if (e.keyCode == '38') {
+			// Up arrow clicked
+		}
+	},
+
+	/* Ajax call */
 	// Examples:
 	// ajax_handler.post("https://my-api.azurewebsites.net/videos", "post", "", callback_fn);
 	// ajax_handler.post("https://my-api.azurewebsites.net/videos", "post");
 	ajax: function(url, type, data, cbk) {
 		$.ajax({
-		url: url,
-		// async: true,
-		// cache: false,
-		// crossDomain: true,
-		type: type,
-		method: type,
-		data: data,
-		datatype: 'json'
+			url: url,
+			// async: true,
+			// cache: false,
+			// crossDomain: true,
+			type: type,
+			method: type,
+			data: data,
+			datatype: 'json'
 		})
 		.done(function(data) {
 			console.log(data);
@@ -195,7 +195,7 @@ var gl = {
 		});
 	},
 
-    /* JS equivalent of php htmlentitites */
+	/* JS equivalent of php htmlentitites */
 	html_entities: function(str) {
 		return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 	}
@@ -207,12 +207,12 @@ $('body').on('click', '.ctc-this', function () {
 });
 
 var date_handler = {
-    today: new Date(),
+	today: new Date(),
 	current_year: date_handler.today.getUTCFullYear(),
 	current_month: date_handler.today.getUTCMonth() + 1, // Months from 1-12
-    current_day: date_handler.today.getUTCDate(),
+	current_day: date_handler.today.getUTCDate(),
 
-    get_date: function(days_offset, as_string, date_separator) { // days_offset [int] (it can also be negative for past dates), as_string [boolean], date_separator [string]
+	get_date: function(days_offset, as_string, date_separator) { // days_offset [int] (it can also be negative for past dates), as_string [boolean], date_separator [string]
 		if(days_offset == undefined) {
 			days_offset = 0;
 		}
@@ -231,7 +231,7 @@ var date_handler = {
 		return date_to_return;
 	},
 
-    seconds_in_period: {
+	seconds_in_period: {
 		"year": 31536000,
 		"month": 2592000, // 86400*30=2592000, 31536000/12=2628000
 		"day": 86400,
@@ -282,7 +282,7 @@ var date_handler = {
 			}
 		});
 
-        return time_passed;
+		return time_passed;
 	}
 }
 
@@ -293,13 +293,13 @@ var share = {
 	 - add onclick(function_name("url_to_share")) to the html element
 	 - add .js-fb-share .js-tw-share .js-li-share classes and data-share-url attribute to the html element
 	*/
- 	facebook:function(url) {
+	facebook:function(url) {
 		var res = encodeURIComponent(url);
 		var fb = "https://www.facebook.com/sharer/sharer.php?u=";
 		var risultato = fb.concat(res);
 		window.open(risultato);
 	},
- 	twitter: function(url) {
+	twitter: function(url) {
 		var res = encodeURIComponent(url);
 		var tw = "https://twitter.com/intent/tweet?text=&url=";
 		var risultato = tw.concat(res);
@@ -343,16 +343,16 @@ $("body").on("click", ".js-tg-share", function() {
 
 // document.ready
 $(document).ready(function() {
-	
+	// Do something
 });
 
 // DOM loaded (but images and css could be not!)
 document.addEventListener('DOMContentLoaded', function() {
-	
+	// Do something
 });
 
 // scroll/resize
-$(window).on('scroll resize', function () {
+$(window).on('scroll resize', function() {
 	gl.lazy_load_picture();
 });
 
@@ -368,7 +368,7 @@ window.onscroll = function() {
 
 // Mousewheel scroll
 $('body').on('mousewheel DOMMouseScroll', function() {
-
+	// Do something
 });
 
 // Keyboard events
