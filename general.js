ae44a7425c66b540b5f764e33e8d2c33736118ca
LucaBn <luca.bonotti@virgilio.it>
2021-05-22 01:01:13 +0200

    Update

--- general.js
+++ general.js
@@ -60,24 +60,62 @@ var cookie_handler = {
 		d.setTime(d.getTime() + (exdays*24*60*60*1000));
 		var expires = "expires="+d.toUTCString();
 		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-		cbk();
+		if(cbk === undefined || cbk == "") {
+			// No callback
+		} else {
+			cbk();
+		}
 	},
 	get: function(cname) {
 		var name = cname + "=";
 		var ca = document.cookie.split(';');
 		for(var i = 0; i < ca.length; i++) {
 			var c = ca[i];
-			while (c.charAt(0) == ' ') {
+			while(c.charAt(0) == ' ') {
 				c = c.substring(1);
 			}
-			if (c.indexOf(name) == 0) {
+			if(c.indexOf(name) == 0) {
 				return c.substring(name.length, c.length);
 			}
 		}
 		return "";
 	},
 	del: function(cname, cbk) {
+		if(cbk === undefined) {
+			cbk == ""
+		}
 		cookie_handler.set(cname, "", -1, cbk); // Set it in the past
+	}
+}
+
+/* Storage handler */
+var storage_handler = {
+	set: function(name, value, cbk) {
+		localStorage.setItem(name, value);
+		if(cbk === undefined || cbk == "") {
+			// No callback
+		} else {
+			cbk();
+		}
+	},
+	get: function(name) {
+		return localStorage.getItem(name);
+	},
+	del: function(name, cbk) {
+		localStorage.removeItem(name);
+		if(cbk === undefined || cbk == "") {
+			// No callback
+		} else {
+			cbk();
+		}
+	},
+	del_all: function(cbk) {
+		localStorage.clear();
+		if(cbk === undefined || cbk == "") {
+			// No callback
+		} else {
+			cbk();
+		}
 	}
 }
 
@@ -96,6 +134,13 @@ var urls_handler = {
 var gl = {
 	// Detect if tablet
 	is_tablet: /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(navigator.userAgent.toLowerCase()),
+
+	// Detect if mobile
+	is_mobile: function() {
+		let check = false;
+		(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+		return check;
+	}(),
 
 	// Detect if IE
 	is_ie: !!document.documentMode,
@@ -181,7 +226,7 @@ var gl = {
 			datatype: 'json'
 		})
 		.done(function(data) {
-			console.log(data);
+			// console.log(data);
 			if(cbk === undefined || cbk == "") {
 				// No callback
 			} else {
@@ -207,10 +252,14 @@ $('body').on('click', '.ctc-this', function() {
 });
 
 var date_handler = {
-	today: new Date(),
-	current_year: date_handler.today.getUTCFullYear(),
-	current_month: date_handler.today.getUTCMonth() + 1, // Months from 1-12
-	current_day: date_handler.today.getUTCDate(),
+	today: Date.now(),
+	current_year: new Date().getUTCFullYear(),
+	current_month: new Date().getUTCMonth() + 1, // Months from 1-12
+	current_day: new Date().getUTCDate(),
+
+	update_today: function() { // Update date_handler.today
+		date_handler.today = Date.now();
+	},
 
 	get_date: function(days_offset, as_string, date_separator) { // days_offset [int] (it can also be negative for past dates), as_string [boolean], date_separator [string]
 		if(days_offset == undefined) {
@@ -247,6 +296,7 @@ var date_handler = {
 			"hour": ["hour", "hours"],
 			"minute": ["minute", "minutes"],
 			"second": ["second", "seconds"],
+			"ago": "ago"
 		},
 		"it": {
 			"year": ["anno", "anni"],
@@ -255,34 +305,32 @@ var date_handler = {
 			"hour": ["ora", "ore"],
 			"minute": ["minuto", "minuti"],
 			"second": ["secondo", "secondi"],
-		}
-	},
-	get_time_since_lang_utility: {
-		"en": {
-			"ago": "ago"
-		},
-		"it": {
 			"ago": "fa"
 		}
 	},
-	get_time_since: function(date) { // Parameter date must be timestamp in seconds
-		var time_passed = "";
+	get_time_since: function(date, lang) { // Parameter date must be timestamp in seconds
+		if(lang == undefined || date_handler.time_dictionary[lang] == undefined) {
+			lang = "it";
+		}
+
+		var time_passed = -1;
 		var interval = 0;
 		var time_diff = ((Date.now())/1000) - (parseInt(date));
 		var seconds = Math.floor(time_diff);
 
-		$.each(gl.seconds_in_period, function(key, value) {
+		$.each(date_handler.seconds_in_period, function(key, value) {
 			interval = seconds / value;
-			if (interval > 1 && time_passed == "") {
+			if(interval > 1) {
 				if(Math.floor(interval) == 1) {
-					time_passed = Math.floor(interval) + " " + gl.time_dictionary["it"][key][0] + " " + gl.get_time_since_lang_utility["it"]["ago"];
+					time_passed = Math.floor(interval) + " " + date_handler.time_dictionary[lang][key][0] + " " + date_handler.time_dictionary[lang]["ago"]; // Singular
 				} else {
-					time_passed = Math.floor(interval) + " " + gl.time_dictionary["it"][key][1] + " " + gl.get_time_since_lang_utility["it"]["ago"];
+					time_passed = Math.floor(interval) + " " + date_handler.time_dictionary[lang][key][1] + " " + date_handler.time_dictionary[lang]["ago"]; // Plural
 				}
+				return false; // This breaks the loop
 			}
 		});
 
-		return time_passed;
+		return time_passed; // If it returns -1 variable date value is future
 	}
 }
 
@@ -336,19 +384,37 @@ $("body").on("click", ".js-tg-share", function() {
 	share.telegram($(this).attr("data-share-url"));
 });
 
-// document.ready
-$(document).ready(function() {
-	// Do something
-});
+function get_all_obj() {
+	console.log("var scroll_to_position");
+	console.log(scroll_to_position);
+	console.log("var cookie_handler");
+	console.log(cookie_handler);
+	console.log("var urls_handler");
+	console.log(urls_handler);
+	console.log("var gl");
+	console.log(gl);
+	console.log("var date_handler");
+	console.log(date_handler);
+	console.log("var share");
+	console.log(share);
+	console.log("var animation");
+	console.log(animation);
+}
 
 // DOM loaded (but images and css could be not!)
 document.addEventListener('DOMContentLoaded', function() {
-	// Do something
+	console.log("Document is fully loaded");
+});
+
+// document.ready
+$(document).ready(function() {
+	console.log("Document is ready");
+	console.log("%ci%c Run get_all_obj() to get a list of all available objects and their methods", "color:#fff;background:blue;border-radius:2px;padding-left:5px;padding-right:5px;", "");
 });
 
 // scroll/resize
 $(window).on('scroll resize', function() {
-	gl.lazy_load_picture();
+	//
 });
 
 // Only scroll, detecting direction
